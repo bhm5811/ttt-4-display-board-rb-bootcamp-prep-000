1d3cfa395be761fd1ea75a7d660dcd893dbf20b1
Hyemi Bae <bhm5811@naver.com>
2018-02-20 03:02:33 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(board)
-  puts "#{board[0]}.center(3)|#{board[1]}.center(3)|#{board[2]}.center(3)"
+  puts "#{board[0]}".center(3) + "|" + "#{board[1]}".center(3) + "|" + "#{board[2]}".center(3)
   puts "-----------"
   puts "#{board[3]}.center(3)|#{board[4]}.center(3)|#{board[5]}.center(3)"
   puts "-----------"
