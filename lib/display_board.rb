b92b34c26517b244c035a602b06b5e973b64d43d
Hyemi Bae <bhm5811@naver.com>
2018-02-20 03:03:09 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,9 +3,9 @@
 def display_board(board)
   puts "#{board[0]}".center(3) + "|" + "#{board[1]}".center(3) + "|" + "#{board[2]}".center(3)
   puts "-----------"
-  puts "#{board[3]}.center(3)|#{board[4]}.center(3)|#{board[5]}.center(3)"
+  puts "#{board[3]}".center(3) + "|" + "#{board[4]}".center(3) + "|" + "#{board[5]}".center(3)
   puts "-----------"
-  puts "#{board[6]}.center(3)|#{board[7]}.center(3)|#{board[8]}.center(3)"
+  puts "#{board[6]}".center(3) + "|" + "#{board[7]}".center(3) + "|" + "#{board[8]}".center(3)
 end
 
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
