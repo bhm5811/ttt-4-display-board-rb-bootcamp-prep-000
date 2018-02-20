1c55c90a43f728ff6d3df88e333da229f64bb35c
Hyemi Bae <bhm5811@naver.com>
2018-02-20 02:53:45 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,11 +1,11 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(board)
-  puts "\n#{board[0]}\n|\n#{board[1]}\n|\n#{board[2]}\n"
+  puts "\t#{board[0]}\t|\t#{board[1]}\t|\t#{board[2]}\t"
   puts "-----------"
-  puts "\n#{board[3]}\n|\n#{board[4]}\n|\n#{board[5]}\n"
+  puts "\t#{board[3]}\t|\t#{board[4]}\t|\t#{board[5]}\t"
   puts "-----------"
-  puts "\n#{board[6]}\n|\n#{board[7]}\n|\n#{board[8]}\n"
+  puts "\t#{board[6]}\t|\t#{board[7]}\t|\t#{board[8]}\t"
 end
 
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
